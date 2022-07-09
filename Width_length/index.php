1a684580f58abee8323fe0b6c267ae05dccc3820
H-Hassan2003 <108080541+H-Hassan2003@users.noreply.github.com>
2022-07-09 13:54:47 +0430

    Three projects together

---
+++ Width_length/index.php
@@ -0,0 +1,15 @@
+<?php
+
+// PHP program to show the area of a retangle as following: 9 * 3
+
+$W = 10;
+$L = 5;
+
+$R = NULL;
+
+
+$R = $W ." * ". $L;
+
+echo "This is a rectangle of this width and length = " . $R . " Units";
+
+?>
